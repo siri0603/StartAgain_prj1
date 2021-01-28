6aefc73fb4c60f274bbd12bfc0c41efb33a24bcb
siri0603 <nagasireesha@yahoo.com>
2021-01-28 18:34:48 -0500

    Firstjavaclass

---
+++ src/main/java/Class1.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.jar.JarOutputStream;
+
+
+public class Class1 {
+
+    public static void main(String args[])
+    {
+        WebDriver driver;
+        System.out.println("Hello");
+        System.setProperty("webdriver.chrome.driver","C:\\Tools_Download\\Drivers\\chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.get("http://www.google.com/");
+        driver.quit();
+
+
+    }
+
+}
